a6f08c3d39097b3a283f84142edcb0d5938dbde7
camilaseasky <camila.seasky@gmail.com>
2020-05-21 16:55:58 -0400

    finalizacao desafio

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const emailFound = await this.customersRepository.findByEmail(email);
+
+    if (emailFound) {
+      throw new AppError('Email is used for other customer');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,71 @@ interface IRequest {
 @injectable()
 class CreateProductService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    // validando customer
+    const customerValidated = await this.customersRepository.findById(
+      customer_id,
+    );
+
+    if (!customerValidated) {
+      throw new AppError('Custormer not exists');
+    }
+
+    // validando products
+    const productIds = products.map(product => {
+      return { id: product.id };
+    });
+    const productsValidated = await this.productsRepository.findAllById(
+      productIds,
+    );
+
+    if (productsValidated.length < products.length) {
+      throw new AppError('There are invalids products in your order');
+    }
+
+    // validando as quantidades em estoque dos produtos
+    const productsStock = productsValidated.map(prodValidated => {
+      const prodOrder = products.find(prod => prod.id === prodValidated.id);
+
+      return {
+        id: prodValidated.id,
+        quantity: prodValidated.quantity - (prodOrder?.quantity || 0),
+      };
+    });
+
+    if (productsStock.find(prod => prod.quantity < 0)) {
+      throw new AppError('There are products with insuficient stock');
+    }
+
+    const productsOrder = productsValidated.map(prodValidated => {
+      const prodOrder = products.find(prod => prod.id === prodValidated.id);
+
+      return {
+        product_id: prodValidated.id,
+        price: prodValidated.price,
+        quantity: prodOrder?.quantity || 0,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer: customerValidated,
+      products: productsOrder,
+    });
+
+    // Atualizando as quantidades
+    await this.productsRepository.updateQuantity(productsStock);
+
+    return order;
   }
 }
 
